2afd01b33d429e6d6ca6f324ab4e93bf17d38672
LukeDeKiewitBitcube <79079523+lukedekiewit@users.noreply.github.com>
2025-12-24 22:02:14 +0200

    update

--- src/components/custom/InteractiveCVPageHeader.tsx
+++ src/components/custom/InteractiveCVPageHeader.tsx
@@ -1,12 +1,17 @@
 "use client";
 
-import { CheckCircle, Download, Share } from "lucide-react";
+import {
+  CheckCircle,
+  DownloadIcon,
+  PrinterIcon,
+  ShareIcon,
+  SmileIcon,
+} from "lucide-react";
 import { toast } from "sonner";
 import { Avatar, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
 import { Separator } from "../ui/separator";
-import { Toaster } from "../ui/sonner";
-import { ThemeToggle } from "./ThemeToggle";
+import { ThemeSwitcher } from "./ThemeToggle";
 
 interface InteractiveCVPageHeaderProps {
   lightModeLogo?: InteractiveCVPageHeaderLogo;
@@ -20,25 +25,75 @@ interface InteractiveCVPageHeaderLogo {
   alt: string;
 }
 
+const copyLink = async (shareLink: string) => {
+  if (typeof window === "undefined") {
+    console.error("Could not open copy link: window is undefined");
+    toast.error(
+      <span className="font-mono">Error: Could not open print dialog</span>,
+    );
+  }
+
+  try {
+    await navigator.clipboard.writeText(shareLink);
+
+    try {
+      const clipboardContent = await navigator.clipboard.readText();
+
+      if (clipboardContent === shareLink) {
+        toast(
+          <div className="flex flex-col gap-1 font-mono">
+            <span className="text-primary uppercase select-all">
+              {shareLink}
+            </span>
+            <div className="flex items-center gap-2">
+              <CheckCircle className="size-4" />
+              <span>Link copied to clipboard!</span>
+            </div>
+          </div>,
+        );
+      }
+    } catch (error) {
+      console.error("Could not copy link:", error);
+      toast.error(
+        <span className="font-mono">Error: Could not copy link</span>,
+      );
+    }
+  } catch (error) {
+    console.error("Could not copy link:", error);
+    toast.error(<span className="font-mono">Error: Could not copy link</span>);
+  }
+};
+
+const printPage = async () => {
+  if (typeof window === "undefined") {
+    console.error("Could not open print dialog: window is undefined");
+    return await toast.error(
+      <span className="font-mono">Error: Could not open print dialog</span>,
+    );
+  }
+
+  try {
+    await window.print();
+    toast(
+      <div className="flex items-center gap-2">
+        <SmileIcon className="size-4" />
+        <span>Thank you!</span>
+      </div>,
+    );
+  } catch (error) {
+    console.error("Could not open print dialog:", error);
+    toast.error(
+      <span className="font-mono">Error: Could not open print dialog</span>,
+    );
+  }
+};
+
 export const InteractiveCVPageHeader = ({
   lightModeLogo,
   darkModeLogo,
   lastUpdated,
   shareLink,
 }: InteractiveCVPageHeaderProps) => {
-  const copyLink = () => {
-    navigator.clipboard.writeText(shareLink);
-    toast(
-      <div className="flex flex-col gap-1">
-        <span className="text-primary uppercase select-all">{shareLink}</span>
-        <div className="flex items-center gap-2">
-          <CheckCircle className="size-4" />
-          <span>Link copied to clipboard!</span>
-        </div>
-      </div>,
-    );
-  };
-
   return (
     <div className="bg-card flex flex-col print:hidden">
       <div className="flex items-center justify-between gap-4 p-4 sm:p-4 md:flex-row md:p-6 lg:flex-row lg:p-8">
@@ -53,36 +108,45 @@ export const InteractiveCVPageHeader = ({
               <AvatarImage src={darkModeLogo.src} alt={darkModeLogo.alt} />
             </Avatar>
           )}
-          <div className="text-foreground/60 flex flex-col text-sm">
+          <div
+            className="text-foreground/60 flex flex-col"
+            style={{
+              fontSize: "clamp(0.7rem, 0.8rem, 0.9rem)",
+            }}
+          >
             <span className="align-middle">Last Updated:</span>
             <time className="align-middle">{lastUpdated}</time>
           </div>
         </div>
         <div className="flex gap-2">
-          <Button
-            variant={"default"}
-            aria-label="Download CV"
-            title="Click to download CV"
-          >
-            <Download />
+          <Button variant={"default"} title="Download this CV in PDF format">
+            <DownloadIcon />
             <span className="hidden sm:hidden md:inline-flex">Download</span>
           </Button>
           <Button
             variant={"outline"}
-            aria-label="Share this page (copy link)"
             title="Click to copy the link to this page to your clipboard"
             onClick={() => {
-              copyLink();
+              copyLink(shareLink);
             }}
           >
-            <Share />
+            <ShareIcon />
             <span className="hidden sm:hidden md:inline-flex">Share</span>
           </Button>
-          <ThemeToggle />
+          <Button
+            variant={"outline"}
+            title="Print this page"
+            onClick={() => {
+              printPage();
+            }}
+          >
+            <PrinterIcon />
+            <span className="hidden sm:hidden md:inline-flex">Print</span>
+          </Button>
+          <ThemeSwitcher />
         </div>
       </div>
       <Separator className="bg-muted border" decorative />
-      <Toaster position="bottom-center" />
     </div>
   );
 };
--- src/components/custom/ThemeToggle.tsx
+++ src/components/custom/ThemeToggle.tsx
@@ -1,4 +1,6 @@
-import { Laptop, Moon, Sun } from "lucide-react";
+"use client";
+
+import { LaptopIcon, MoonIcon, PaintbrushIcon, SunIcon } from "lucide-react";
 import { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import {
@@ -7,24 +9,66 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { toast } from "sonner";
 
-export const ThemeToggle = () => {
-  const [theme, setThemeState] = useState<"theme-light" | "dark" | "system">(
-    "theme-light",
-  );
+type Themes = "theme-light" | "dark" | "system";
+
+export const ThemeSwitcher = () => {
+  const [currentTheme, setCurrentTheme] = useState<Themes>("theme-light");
+
+  const switchTheme = (theme: Themes) => {
+    setCurrentTheme(theme);
+    if (theme === currentTheme) {
+      return;
+    }
+    switch (theme) {
+      case "theme-light":
+        toast(
+          <div>
+            <div className="flex items-center gap-2">
+              <PaintbrushIcon className="text-primary size-4" />
+              <span>Theme: Light</span>
+            </div>
+          </div>,
+        );
+        break;
+
+      case "dark":
+        toast(
+          <div>
+            <div className="flex items-center gap-2">
+              <PaintbrushIcon className="text-primary size-4" />
+              <span>Theme: Dark</span>
+            </div>
+          </div>,
+        );
+        break;
+
+      case "system":
+        toast(
+          <div>
+            <div className="flex items-center gap-2">
+              <PaintbrushIcon className="text-primary size-4" />
+              <span>Theme: System</span>
+            </div>
+          </div>,
+        );
+        break;
+    }
+  };
 
   useEffect(() => {
     const isDarkMode = document.documentElement.classList.contains("dark");
-    setThemeState(isDarkMode ? "dark" : "theme-light");
+    setCurrentTheme(isDarkMode ? "dark" : "theme-light");
   }, []);
 
   useEffect(() => {
     const isDark =
-      theme === "dark" ||
-      (theme === "system" &&
+      currentTheme === "dark" ||
+      (currentTheme === "system" &&
         window.matchMedia("(prefers-color-scheme: dark)").matches);
     document.documentElement.classList[isDark ? "add" : "remove"]("dark");
-  }, [theme]);
+  }, [currentTheme]);
 
   return (
     <DropdownMenu>
@@ -34,33 +78,33 @@ export const ThemeToggle = () => {
           size="icon"
           title="Click to choose a different theme"
         >
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <SunIcon className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem
           className="cursor-pointer text-sm"
-          onClick={() => setThemeState("theme-light")}
+          onClick={() => switchTheme("theme-light")}
           title="Light theme"
         >
-          <Sun className="text-primary" />
+          <SunIcon className="text-primary" />
           <span>Light</span>
         </DropdownMenuItem>
         <DropdownMenuItem
           className="cursor-pointer text-sm"
-          onClick={() => setThemeState("dark")}
+          onClick={() => switchTheme("dark")}
           title="Dark theme"
         >
-          <Moon className="text-primary" /> <span>Dark</span>
+          <MoonIcon className="text-primary" /> <span>Dark</span>
         </DropdownMenuItem>
         <DropdownMenuItem
           className="cursor-pointer text-sm"
-          onClick={() => setThemeState("system")}
+          onClick={() => switchTheme("system")}
           title="System theme"
         >
-          <Laptop className="text-primary" /> <span>System</span>
+          <LaptopIcon className="text-primary" /> <span>System</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
